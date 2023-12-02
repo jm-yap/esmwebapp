9f09c7314c9ddf97c65d91d9bf4a626c96ece74f
Ryan Decena <rpdecena@up.edu.ph>
2023-12-02 16:17:13 +0800

    modified fetch and implemented add

--- actions/surveymodule.ts
+++ actions/surveymodule.ts
@@ -1,22 +1,44 @@
 'use server';
-
 import {
     collection,
     getDocs,
     QueryDocumentSnapshot,
+    addDoc
 } from 'firebase/firestore';
 import { db } from '../firebase';
 
-export async function getSurveyModules() {
+type SurveyModule = {
+    id: string;
+    data: {
+        isAnonymous: boolean;
+    };
+};
+
+export async function getSurveyModules() : Promise<any> {
     const surveyModuleCollection = collection(db, 'ResearchModule');
     const surveyModuleSnapshot = await getDocs(surveyModuleCollection);
 
     const surveyModuleList = surveyModuleSnapshot.docs.map((doc: QueryDocumentSnapshot) => {
         return {
             id: doc.id,
-            data: doc.data(),
+            data: doc.data() as { isAnonymous: boolean },
         };
     });
 
     return surveyModuleList;
+}
+
+export async function addSurveyModule(isSurveyModuleAnonymous: boolean) {
+    try {
+        const surveyModuleCollection = collection(db, 'ResearchModule');
+        const newSurveyModule = await addDoc(surveyModuleCollection, {
+            isAnonymous: isSurveyModuleAnonymous
+        });
+        
+        console.log("New survey module created with ID: ", newSurveyModule.id);
+        return true;
+    } catch (error) {
+        console.error("Error creating new survey module", error);
+        return false;
+    }
 }
--- app/surveymodule/page.tsx
+++ app/surveymodule/page.tsx
@@ -1,19 +1,51 @@
-import { getSurveyModules } from '@/actions/surveymodule'
+"use client";
+import React, { useState, useEffect} from 'react'
+import { getSurveyModules, addSurveyModule } from '@/actions/surveymodule'
 
-export default async function surveyModule() {
-    const surveyModules = await getSurveyModules(); // Get the list of survey modules
+export default function surveyModule() {
+    const [surveyModules, setSurveyModules] = useState([]); // Get the list of survey modules
+    const [isAnonymous, setIsAnonymous] = useState(false); // Get the list of survey modules
+    
+    useEffect(() => {
+        const fetchData = () => {
+            getSurveyModules().then((modules) => {
+                setSurveyModules(modules);
+            });
+        };
+
+        fetchData();
+    }, []);
+
+    const handleIsNotAnonymous = async () => {
+        try {
+            await addSurveyModule(isAnonymous);
+        } catch (error: any) {
+            console.error("Error adding survey module:", error.message);
+        }
+    };
+
+    const handleIsAnonymous = async () => {
+        try {
+            await addSurveyModule(true);
+        } catch (error: any) {
+            console.error("Error adding survey module:", error.message);
+        }
+    };
 
     return (
         <div>
             <h1>Survey Module</h1>
             <ul>
-                {surveyModules.map((surveyModule) => (
+                {surveyModules.map((surveyModule: { id: string, data: { isAnonymous: boolean } }) => (
                     <li key={surveyModule.id}>
-                        ClientID: {surveyModule.data.ClientID} <br />
-                        IsAnonymous: {surveyModule.data.IsAnonymous ? 'Yes' : 'No'} <br />
+                        {surveyModule.id}
                     </li>
                 ))}
             </ul>
+            <p>Create Survey Module</p>
+            <p>Is this Survey Module Anonymous?</p>
+            <button onClick={handleIsAnonymous}>Yes</button>
+            <button onClick={handleIsNotAnonymous}>No</button>
         </div>
     );
 }

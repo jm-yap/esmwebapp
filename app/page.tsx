29cdfabdbda73723787027d1be442e766ed4890a
jm-yap <jelamay.yap@gmail.com>
2024-05-10 02:16:02 +0800

    login loading

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,16 @@ import { useRouter, redirect } from "next/navigation";
 import { fetchMasterKey } from "@/actions/masterkey";
 import styles from "./styles.module.css";
 import { useSession, signOut } from "next-auth/react";
+import { on } from "events";
+import CircularProgress from '@mui/material/CircularProgress';
+import Stack from '@mui/material/Stack';
+import { set } from "firebase/database";
 
 function HomePage() {
   const router = useRouter();
   const [masterKey, setMasterKey] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setLoading] = useState(true);
   const session = useSession({
     required: true,
     onUnauthenticated() {
@@ -27,6 +32,7 @@ function HomePage() {
         sessionStorage.setItem("masterKey", "true");
         console.log("Master key is correct");
         sessionStorage.setItem("userEmail", session.data.user?.email);
+        setLoading(false);
         sessionStorage.setItem("validInfo", "true");
         router.push("/surveymodule");
         
@@ -41,33 +47,44 @@ function HomePage() {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.centerBox}>
-        <div className={styles.titleBox}>
-          <div className={styles.titleWebPage}>
-            <h1>Sagot</h1><h1 className={styles.titleOrange}>Kita</h1><h1>.</h1>
-            
+    <div>
+      {isLoading ? 
+        <div className={styles.loadingContainer}> 
+        <Stack sx={{ color: '#E07961' }} spacing={2} direction="row">
+          <CircularProgress color="inherit" size={50}/>
+        </Stack>
+        </div> :
+        <div>
+          <div className={styles.container}>
+            <div className={styles.centerBox}>
+              <div className={styles.titleBox}>
+                <div className={styles.titleWebPage}>
+                  <h1>Sagot</h1><h1 className={styles.titleOrange}>Kita</h1><h1>.</h1>
+                  
+                </div>
+              </div>
+              <div className={styles.titleBox}>
+                <form onSubmit={handleMasterKeySubmit}>
+                  <div className={styles.masterKeyForm}>
+                    <h1 className={styles.masterKeyText}>Masterkey</h1>
+                    <input
+                      id="masterKeyInput"
+                      className={styles.inputBox}
+                      type="password"
+                      value={masterKey}
+                      onChange={(e) => setMasterKey(e.target.value)}
+                    />
+                    {error && <p  className={styles.errorText}>{error}</p>}
+                    <button className={styles.submitButton} type="submit"> 
+                      N E X T
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
-        <div className={styles.titleBox}>
-          <form onSubmit={handleMasterKeySubmit}>
-            <div className={styles.masterKeyForm}>
-              <h1 className={styles.masterKeyText}>Masterkey</h1>
-              <input
-                id="masterKeyInput"
-                className={styles.inputBox}
-                type="password"
-                value={masterKey}
-                onChange={(e) => setMasterKey(e.target.value)}
-              />
-              {error && <p  className={styles.errorText}>{error}</p>}
-              <button className={styles.submitButton} type="submit"> 
-                N E X T
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      }
     </div>
   );
 }

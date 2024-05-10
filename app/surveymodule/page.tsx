d9f14a09d74a8d897f28b1938826a04cfd2d4eda
VDispo <vdispo@up.edu.ph>
2024-05-10 10:14:24 +0800

    fixed loading of surveymodule

--- app/surveymodule/page.tsx
+++ app/surveymodule/page.tsx
@@ -10,6 +10,7 @@ import { useSession, signOut } from "next-auth/react";
 import { redirect, useRouter } from "next/navigation";
 import styles from "@/app/surveymodule/styles.module.css";
 import Link from "next/link";
+import CircularProgress from '@mui/material/CircularProgress';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { getClientAccountByEmail } from "@/actions/clients";
@@ -99,7 +100,6 @@ export default function SurveyModule() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session]);
 
-
   // useEffect(() => {
   //   const fetchSurveyCounts = async () => {
   //     const counts = await Promise.all(
@@ -155,103 +155,112 @@ export default function SurveyModule() {
   };
 
   return (
-    <div className={styles.container}>
-      {/* <div> */}
-        <div className={styles.navbar}>
-          <Link href="/surveymodule" className={styles.navtext} onClick={handleClick}>
-            <h1 className={styles.navblack}>Sagot</h1>
-            <h1 className={styles.navwhite}>Kita</h1>
-            <h1 className={styles.navblack}>.</h1>
-          </Link>
-          <Link href="/builderprofile" className={styles.navprofilecontainer} onClick={handleClick}>
-            <h1 className={styles.navinfotext}>{firstName} {lastName}</h1>
-            <AccountCircleIcon fontSize="large" />
-          </Link>
-        </div>
-        {/* {isLoading &&
-          <div className={styles.loading}>
-            <Stack sx={{ width: '100%', color: '#cf6851' }} spacing={2}>
-              <LinearProgress color="inherit"  sx={{ width: '100%', height: '7px' }}/> 
-            </Stack>
-          </div>
-        } */}
-      {/* </div> */}
-
-      <div className={styles.sidebar}>
-        <div className={styles.sidebarContent}>
-          <div className={styles.sidebarTitleContainer}>
-            <h1 className={styles.sidebarTitle}>Create Survey Module</h1>
-          </div>
-          
-          <div className={styles.sidebarForm}>
-            <form className={styles.sidebarFormComp} onSubmit={handleAddSurveyModule}>
-              <div className={styles.sidebarFormBit}>
-                <label className={styles.sidebarLabel}>Title</label>
-                <input type="text" name="title" className={styles.sidebarTextField} required/>
+    <div>
+      {!verified ?
+        <div className={styles.loadingContainer}> 
+          <Stack sx={{ color: '#E07961' }} spacing={2} direction="row">
+            <CircularProgress color="inherit" size={50}/>
+          </Stack>
+        </div>:
+        <div className={styles.container}>
+          {/* <div> */}
+            <div className={styles.navbar}>
+              <Link href="/surveymodule" className={styles.navtext} onClick={handleClick}>
+                <h1 className={styles.navblack}>Sagot</h1>
+                <h1 className={styles.navwhite}>Kita</h1>
+                <h1 className={styles.navblack}>.</h1>
+              </Link>
+              <Link href="/builderprofile" className={styles.navprofilecontainer} onClick={handleClick}>
+                <h1 className={styles.navinfotext}>{firstName} {lastName}</h1>
+                <AccountCircleIcon fontSize="large" />
+              </Link>
+            </div>
+            {/* {isLoading &&
+              <div className={styles.loading}>
+                <Stack sx={{ width: '100%', color: '#cf6851' }} spacing={2}>
+                  <LinearProgress color="inherit"  sx={{ width: '100%', height: '7px' }}/> 
+                </Stack>
               </div>
+            } */}
+          {/* </div> */}
 
-              <div className={styles.sidebarFormBit}>
-                <label className={styles.sidebarLabel}>Description</label>
-                <textarea rows={2}  name="description" className={styles.sidebarTextField} required/>
-              </div>
-
-              <div className={styles.sidebarFormBit}>
-                <FormControlLabel 
-                  control={<Checkbox
-                      onChange={handleCheckboxChange} 
-                      sx={{'&.Mui-checked': {color: '#E07961'}}}
-                    />}
-                  label="Are Clients Anonymous"
-                />
+          <div className={styles.sidebar}>
+            <div className={styles.sidebarContent}>
+              <div className={styles.sidebarTitleContainer}>
+                <h1 className={styles.sidebarTitle}>Create Survey Module</h1>
               </div>
               
-              <button className={styles.sidebarButton} type="submit">C R E A T E</button>
-            </form>
-          </div>
-        </div>
-      </div>
-      
-      <main className={styles.main}>
-        <div style={{position: 'fixed', width: '100%'}}>
-          {isLoading && (
-            <div style={{ marginTop: '-20px', marginLeft: '-20px' }}>
-              <Stack sx={{ width: '100%', color: '#cf6851' }} spacing={2}>
-                <LinearProgress color="inherit" sx={{ width: '100%', height: '7px' }} />
-              </Stack>
-            </div>
-          )}
-        </div>
-        {surveyModules.length === 0 && !isLoading &&
-          <div className={styles.empty}>
-            <AutoAwesomeIcon sx={{ fontSize: 100, color: '#ddd' }} style={{marginBottom: '20px'}}/>
-            <h1>No modules yet. Create one on the left!</h1>
-          </div>
-        }
-        {verified && surveyModules.map((surveyModule: any) => (
-          <div key={surveyModule.id}>
-            
-              <div className={styles.SurveyContainer}>
-                <div className={styles.sidebarRow}>
-                  <Link href={`/surveymodule/${surveyModule.id}`} onClick={handleClick}>
-                    <button onClick={() =>
-                      localStorage.setItem("surveyModule", JSON.stringify(surveyModule))} // export survey module details
-                      className={styles.SurveyTitle}>
-                        {surveyModule.data.Title}
-                    </button>
-                  </Link>
-                  <button onClick={() => handleDeleteSurveyModule(surveyModule.id)}>
-                    <DeleteOutlineIcon sx={{ fontSize: 30, color: '#E07961' }}/>
-                  </button>
-                </div>
-                <h1 className={styles.SurveyDescription}>{surveyModule.data.Description}</h1>
-                <h1 className={styles.BuilderInfo}>Prepared by: {surveyModule.data.BuilderID}</h1>
+              <div className={styles.sidebarForm}>
+                <form className={styles.sidebarFormComp} onSubmit={handleAddSurveyModule}>
+                  <div className={styles.sidebarFormBit}>
+                    <label className={styles.sidebarLabel}>Title</label>
+                    <input type="text" name="title" className={styles.sidebarTextField} required/>
+                  </div>
+
+                  <div className={styles.sidebarFormBit}>
+                    <label className={styles.sidebarLabel}>Description</label>
+                    <textarea rows={2}  name="description" className={styles.sidebarTextField} required/>
+                  </div>
+
+                  <div className={styles.sidebarFormBit}>
+                    <FormControlLabel 
+                      control={<Checkbox
+                          onChange={handleCheckboxChange} 
+                          sx={{'&.Mui-checked': {color: '#E07961'}}}
+                        />}
+                      label="Are Clients Anonymous"
+                    />
+                  </div>
+                  
+                  <button className={styles.sidebarButton} type="submit">C R E A T E</button>
+                </form>
               </div>
-            
+            </div>
           </div>
-        ))}
-      </main>
-      {/* } */}
-      
+          
+          <main className={styles.main}>
+            <div style={{position: 'fixed', width: '100%'}}>
+              {isLoading && (
+                <div style={{ marginTop: '-20px', marginLeft: '-20px' }}>
+                  <Stack sx={{ width: '100%', color: '#cf6851' }} spacing={2}>
+                    <LinearProgress color="inherit" sx={{ width: '100%', height: '7px' }} />
+                  </Stack>
+                </div>
+              )}
+            </div>
+            {surveyModules.length === 0 && !isLoading &&
+              <div className={styles.empty}>
+                <AutoAwesomeIcon sx={{ fontSize: 100, color: '#ddd' }} style={{marginBottom: '20px'}}/>
+                <h1>No modules yet. Create one on the left!</h1>
+              </div>
+            }
+            {verified && surveyModules.map((surveyModule: any) => (
+              <div key={surveyModule.id}>
+                
+                  <div className={styles.SurveyContainer}>
+                    <div className={styles.sidebarRow}>
+                      <Link href={`/surveymodule/${surveyModule.id}`} onClick={handleClick}>
+                        <button onClick={() =>
+                          localStorage.setItem("surveyModule", JSON.stringify(surveyModule))} // export survey module details
+                          className={styles.SurveyTitle}>
+                            {surveyModule.data.Title}
+                        </button>
+                      </Link>
+                      <button onClick={() => handleDeleteSurveyModule(surveyModule.id)}>
+                        <DeleteOutlineIcon sx={{ fontSize: 30, color: '#E07961' }}/>
+                      </button>
+                    </div>
+                    <h1 className={styles.SurveyDescription}>{surveyModule.data.Description}</h1>
+                    <h1 className={styles.BuilderInfo}>Prepared by: {surveyModule.data.BuilderID}</h1>
+                  </div>
+                
+              </div>
+            ))}
+          </main>
+          {/* } */}
+          
+        </div>
+      }
     </div>
   );
 }

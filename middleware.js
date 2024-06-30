954fedd3c1ca21c45308d4d415bdcadc6f5df9ea
vinceenstack <vincent.dispo@enstack.com>
2024-07-01 00:50:15 +0800

    fix: infinite loop

--- middleware.js
+++ middleware.js
@@ -4,15 +4,11 @@ import { NextResponse } from 'next/server';
 export async function middleware(req) {
     const token = await getToken({ req, secret: process.env.SECRET });
     const { pathname } = req.nextUrl;
-    const protectedPaths = ['/', '/builderprofile', 'editinfo'];
-
-    if (typeof window !== 'undefined') {
-        const masterkey = localStorage.getItem('masterkey');
-    }
+    const protectedPaths = ['/', '/builderprofile', '/editinfo'];
 
     if (token && pathname === '/login') {
         console.log('Token found, redirecting');
-        return NextResponse.redirect(new URL('/surveymodule', req.url));
+        return NextResponse.redirect(new URL('/', req.url));
     }
 
     if (!token && (protectedPaths.includes(pathname) || pathname.startsWith('/surveymodule'))) {

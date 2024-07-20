dcc74f709b29836213493f9a44e42c478656ef60
jm-yap <jelamay.yap@gmail.com>
2024-07-21 03:58:51 +0800

    chore: env vars

--- firebase.js
+++ firebase.js
@@ -1,22 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-import { getFirestore } from "firebase/firestore";
-
-import { getDatabase, ref, get } from "firebase/database";
 
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBNgGIW74VyqwKmnTm6WMXDjgLq4ivQs3E",
-  authDomain: "esm-app-f0711.firebaseapp.com",
-  projectId: "esm-app-f0711",
-  storageBucket: "esm-app-f0711.appspot.com",
-  messagingSenderId: "383934678773",
-  appId: "1:383934678773:web:99c916c945713950cea7bc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

b3eefb7589a24c1f6d587a4938f9a9745c03c277
jm-yap <jelamay.yap@gmail.com>
2024-07-21 15:10:22 +0800

    fix: firebase

--- firebase.js
+++ firebase.js
@@ -1,17 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBNgGIW74VyqwKmnTm6WMXDjgLq4ivQs3E",
+  authDomain: "esm-app-f0711.firebaseapp.com",
+  projectId: "esm-app-f0711",
+  storageBucket: "esm-app-f0711.appspot.com",
+  messagingSenderId: "383934678773",
+  appId: "1:383934678773:web:99c916c945713950cea7bc",
 };
 
 // Initialize Firebase
